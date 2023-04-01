2e8965256412daff02d8a7403c77278f61771da3
Arkadiusz Bojara <bojara.arkadiusz@gmail.com>
2023-04-01 12:27:44 +0200

    Uzupełnienie @RestControllerAdvice o metody MethodArgumentNotValidException.class HttpStatus.BAD_REQUEST

--- src/main/java/com/codecool/dogmate/controller/advice/ErrorControllerAdvice.java
+++ src/main/java/com/codecool/dogmate/controller/advice/ErrorControllerAdvice.java
@@ -3,9 +3,12 @@ package com.codecool.dogmate.controller.advice;
 import com.codecool.dogmate.controller.advice.Exceptions.ProvincesNotFoundException;
 import com.codecool.dogmate.controller.advice.Exceptions.VoivodeshipsNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ErrorControllerAdvice {
@@ -16,7 +19,16 @@ public class ErrorControllerAdvice {
         return  new ErrorResponse(exception.getMessage());
     }
 
-    private record ErrorResponse(String errorMessage) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ErrorResponse handleBadRequest(MethodArgumentNotValidException ex) {
+        String errMsg = ex.getAllErrors().stream()
+                .map(e -> e.getDefaultMessage())
+                .collect(Collectors.joining(" | "));
 
+        return new ErrorResponse(errMsg);
+    }
+
+    private record ErrorResponse(String errorMessage) {
     }
 }
